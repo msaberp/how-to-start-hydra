a23a3ec0db53fa476213befd760830a01427e65b
msaberp <m.saberpourheydari@gmail.com>
2021-10-17 18:53:41 +0200

    lesson 1: managing the arguments with sys

---
+++ lessons/lesson_1.py
@@ -0,0 +1,22 @@
+import sys
+
+"""
+An example of managing arguments using sys library.
+
+input:
+>>> python lessons/lesson_1.py arg1 arg2 arg3
+
+output:
+>>> This is the name of the program: lessons/lesson_1.py
+>>> This is the 1th argument: arg1
+>>> This is the 2th argument: arg2
+>>> This is the 3th argument: arg3
+
+"""
+
+for arg_idx, arg in enumerate(sys.argv):
+    if arg_idx == 0:
+        print(f"This is the name of the program: {arg}")
+    else:
+        print(f"This is the {arg_idx}th argument: {arg}")
+
